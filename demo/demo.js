e702ffdbefa6863910b517213812367894e50568
John Chan <johntcchan@gmail.com>
2017-01-03 17:08:02 -0800

    Added demo buttons to switch styles

---
+++ demo/demo.js
@@ -0,0 +1,16 @@
+var styleSwitcher = document.createElement("div")
+styleSwitcher.className("style-switcher");
+document.body.appendChild(styleSwitcher);
+
+var switchStyle = function(){
+  var stylesheet = document.getElementByTagName("link")[0];
+  stylesheet.setAttribute("href", this.dataset.styleSrc);
+};
+
+for(var i = 0; i < 4; i++) {
+  var toggle = document.createElement("a")
+  toggle.className("toggle");
+  toggle.dataset.styleSrc = "style" + i + ".css";
+  styleSwitcher.appendChild(toggle);
+  document.addEventListner("click", switchStyle);
+};
