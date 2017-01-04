82538466902367ee3265be6f685a8c3e5e859fa5
John Chan <johntcchan@gmail.com>
2017-01-03 17:21:22 -0800

    JS debug + toggle clean up

--- demo/demo.js
+++ demo/demo.js
@@ -1,17 +1,17 @@
 var styleSwitcher = document.createElement("div");
-styleSwitcher.className("style-switcher");
+styleSwitcher.className = "style-switcher";
 document.body.appendChild(styleSwitcher);
 
 var switchStyle = function(){
-  var stylesheet = document.getElementByTagName("link")[0];
+  var stylesheet = document.getElementsByTagName("link")[0];
   stylesheet.setAttribute("href", this.dataset.styleSrc);
 };
 
 for(var i = 1; i < 4; i++) {
-  var toggle = document.createElement("a")
-  toggle.className("toggle");
+  var toggle = document.createElement("a");
+  toggle.className = "toggle";
   toggle.innerHTML = "Switch to layout " + i;
-  toggle.dataset.styleSrc = "style" + i + ".css";
+  toggle.dataset.styleSrc = "css/style" + i + ".css";
+  toggle.addEventListener("click", switchStyle);
   styleSwitcher.appendChild(toggle);
-  document.addEventListner("click", switchStyle);
 };
