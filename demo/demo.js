c3366be41ab21b2a4633368600af876ca698fe54
John Chan <johntcchan@gmail.com>
2017-01-03 17:22:43 -0800

    Add document ready

--- demo/demo.js
+++ demo/demo.js
@@ -1,17 +1,21 @@
-var styleSwitcher = document.createElement("div");
-styleSwitcher.className = "style-switcher";
-document.body.appendChild(styleSwitcher);
+document.addEventListener("DOMContentLoaded", function(event) {
 
-var switchStyle = function(){
-  var stylesheet = document.getElementsByTagName("link")[0];
-  stylesheet.setAttribute("href", this.dataset.styleSrc);
-};
+  var styleSwitcher = document.createElement("div");
+  styleSwitcher.className = "style-switcher";
+  document.body.appendChild(styleSwitcher);
 
-for(var i = 1; i < 4; i++) {
-  var toggle = document.createElement("a");
-  toggle.className = "toggle";
-  toggle.innerHTML = "Switch to layout " + i;
-  toggle.dataset.styleSrc = "css/style" + i + ".css";
-  toggle.addEventListener("click", switchStyle);
-  styleSwitcher.appendChild(toggle);
-};
+  var switchStyle = function(){
+    var stylesheet = document.getElementsByTagName("link")[0];
+    stylesheet.setAttribute("href", this.dataset.styleSrc);
+  };
+
+  for(var i = 1; i < 4; i++) {
+    var toggle = document.createElement("a");
+    toggle.className = "toggle";
+    toggle.innerHTML = "Switch to layout " + i;
+    toggle.dataset.styleSrc = "css/style" + i + ".css";
+    toggle.addEventListener("click", switchStyle);
+    styleSwitcher.appendChild(toggle);
+  };
+
+});
