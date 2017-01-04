1f708f7a9486677207713d388f6a0bd5be9015ed
John Chan <johntcchan@gmail.com>
2017-01-03 17:23:54 -0800

    Change loading order, remove document ready

--- demo/demo.js
+++ demo/demo.js
@@ -1,21 +1,17 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+var styleSwitcher = document.createElement("div");
+styleSwitcher.className = "style-switcher";
+document.body.appendChild(styleSwitcher);
 
-  var styleSwitcher = document.createElement("div");
-  styleSwitcher.className = "style-switcher";
-  document.body.appendChild(styleSwitcher);
+var switchStyle = function(){
+  var stylesheet = document.getElementsByTagName("link")[0];
+  stylesheet.setAttribute("href", this.dataset.styleSrc);
+};
 
-  var switchStyle = function(){
-    var stylesheet = document.getElementsByTagName("link")[0];
-    stylesheet.setAttribute("href", this.dataset.styleSrc);
-  };
-
-  for(var i = 1; i < 4; i++) {
-    var toggle = document.createElement("a");
-    toggle.className = "toggle";
-    toggle.innerHTML = "Switch to layout " + i;
-    toggle.dataset.styleSrc = "css/style" + i + ".css";
-    toggle.addEventListener("click", switchStyle);
-    styleSwitcher.appendChild(toggle);
-  };
-
-});
+for(var i = 1; i < 4; i++) {
+  var toggle = document.createElement("a");
+  toggle.className = "toggle";
+  toggle.innerHTML = "Switch to layout " + i;
+  toggle.dataset.styleSrc = "css/style" + i + ".css";
+  toggle.addEventListener("click", switchStyle);
+  styleSwitcher.appendChild(toggle);
+};
